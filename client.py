0db070307f08ce690eeb2e16ca208633bbe8945f
Michael Arabian <michaelarabian@michaels-air.wireless.concordia.ca>
2021-11-03 19:09:53 -0400

    public-ip

--- client.py
+++ client.py
@@ -5,10 +5,9 @@ import os
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096  # send 4096 bytes each time step
 
-# the ip address or hostname of the server, the receiver
+
 host = "172.30.119.48"
 
-# the port, let's use 5001
 port = 5001
 
 # the name of file we want to send, make sure it exists
@@ -23,22 +22,21 @@ print(f"[+] Connecting to {host}:{port}")
 s.connect((host, port))
 print("[+] Connected.")
 
-# send the filename and filesize
+
 s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
 progress = tqdm.tqdm(range(
     filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
 with open(filename, "rb") as f:
     while True:
-        # read the bytes from the file
+
         bytes_read = f.read(BUFFER_SIZE)
         if not bytes_read:
             # file transmitting is done
             break
-        # we use sendall to assure transimission in
-        # busy networks
+
         s.sendall(bytes_read)
         # update the progress bar
         progress.update(len(bytes_read))
-# close the socket
+
 s.close()
