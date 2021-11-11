8e767312d613c1cb09fae2278ca7d743f0edcf67
Michael Arabian <michaelarabian@Michaels-MacBook-Air.local>
2021-11-10 20:15:22 -0500

    added db for server

--- server/server.py
+++ server/server.py
@@ -6,23 +6,26 @@ import string
 from colorama import Fore, Back, Style
 
 
-IP = socket.gethostbyname('127.0.0.1') #Setting the IP adress of the socket
-PORT = 5555 #Setting the Port number (might change)
+IP = socket.gethostbyname('127.0.0.1')  # Setting the IP adress of the socket
+PORT = 4444  # Setting the Port number (might change)
 ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 
-clients = [] #List that stores clients information
+clients = []  # List that stores clients information
 
 SERVER_DATA_PATH = "./"
 
-def handle_client(conn, addr): #Handles client request and response
+
+def handle_client(conn, addr):  # Handles client request and response
     clientCount = 0
 
     while 1:
-        data = conn.recv(SIZE).decode(FORMAT) #Data reveived from the client as a reponse
+        # Data reveived from the client as a reponse
+        data = conn.recv(SIZE).decode(FORMAT)
         data = data.split("@")
-        cmd = data[0] #The first thing the client inputs is a command which is stored in cmd
+        # The first thing the client inputs is a command which is stored in cmd
+        cmd = data[0]
 
         if cmd == "HELP":
             send_data = "OK@"  # OK Command to client
@@ -75,64 +78,79 @@ def handle_client(conn, addr): #Handles client request and response
                 conn.send(send_data.encode(FORMAT))
                 print(" REMOVE-DENIED #", addr[1])
 
-        elif cmd == "PUBLISH": #If the client wants to send a file to the server
-            name, text = data[1], data[2] #Setting the name of the client and the file
-            filepath = os.path.join(SERVER_DATA_PATH, name) 
-            with open(filepath, "w") as f: #opening the file
-                f.write(text) #writing to a file the text
-                print(" 📁 File Received : ", filepath) #Print file received by server
-                send_data = "OK@PUBLISHED #"+str(addr[1]) #Tell client that it was puiblished successfully
+        elif cmd == "PUBLISH":  # If the client wants to send a file to the server
+            # Setting the name of the client and the file
+            name, text = data[1], data[2]
+            filepath = os.path.join(SERVER_DATA_PATH, name)
+            with open(filepath, "w") as f:  # opening the file
+                f.write(text)  # writing to a file the text
+                # Print file received by server
+                print(" 📁 File Received : ", filepath)
+                # Tell client that it was puiblished successfully
+                send_data = "OK@PUBLISHED #"+str(addr[1])
                 conn.send(send_data.encode(FORMAT))
 
-        elif cmd == "REMOVE": #If the client wants to delete a file from the server
+        elif cmd == "REMOVE":  # If the client wants to delete a file from the server
             files = os.listdir(SERVER_DATA_PATH)
             send_data = "OK@"
             filename = str(data[1])
-            if len(files) == 0: #if there are no files in the folder
+            if len(files) == 0:  # if there are no files in the folder
                 send_data += "The server directory is empty"
             else:
-                if filename in files: #if the file exists, remove it
+                if filename in files:  # if the file exists, remove it
                     os.remove("./"+filename)
                     send_data += "REMOVED #"+str(addr[1])
                     conn.send(send_data.encode(FORMAT))
 
-        
-        elif cmd == "REGISTER": #If the client command is REGISTER, frist thing the client does
-            clientCount += 1 #Increment client count
-            if data[1] not in clients: #If the clients Name doesnt already exist in the list of clients, register them
+        elif cmd == "REGISTER":  # If the client command is REGISTER, frist thing the client does
+            clientCount += 1  # Increment client count
+            # If the clients Name doesnt already exist in the list of clients, register them
+            if data[1] not in clients:
                 send_data = "OK@"
-                send_data += "REGISTERED # " + str(addr[1]) # Send to the client that they registred with this unique rq number
-                conn.send(send_data.encode(FORMAT)) #Encode the data and send it
+                # Send to the client that they registred with this unique rq number
+                send_data += "REGISTERED # " + str(addr[1])
+                # Encode the data and send it
+                conn.send(send_data.encode(FORMAT))
 
-                name = data[1] #Store the clients name in name
-                IP = data[2] #Store the clients ip address
-                UDP = data[3] #Store the clients udp port
-                TCP = data[4] #Store the clients tcp port
-                print(f" 🚨 [ NEW CONNECTION ] {addr} connected. ") #Print hat there is a new connection with the ip and rq#
-                print(data[0], ' ', '#', addr[1], ' ', data[1], #Print all the inputed user informartion
+                name = data[1]  # Store the clients name in name
+                IP = data[2]  # Store the clients ip address
+                UDP = data[3]  # Store the clients udp port
+                TCP = data[4]  # Store the clients tcp port
+
+                fileName = "./db/" + name + ".txt"
+                f = open(fileName, "x")
+                f.write(name+"\n")
+                f.write(IP+"\n")
+                f.write(UDP+"\n")
+                f.write(TCP+"\n")
+                f.close()
+
+                # Print hat there is a new connection with the ip and rq#
+                print(f" 🚨 [ NEW CONNECTION ] {addr} connected. ")
+                print(data[0], ' ', '#', addr[1], ' ', data[1],  # Print all the inputed user informartion
                       ' ', data[2], ' ', data[3], ' ', data[4])
                 clientString = str(data[1])
-                clients.append(clientString) #Adds client name to list
+                clients.append(clientString)  # Adds client name to list
 
-            else: #If the clients name is already in the list of clients, deny him
-                send_data = "RD@" #Send to the client that they are denied and the reson why
+            else:  # If the clients name is already in the list of clients, deny him
+                send_data = "RD@"  # Send to the client that they are denied and the reson why
                 send_data += "REGISTER-DENIED  # " + \
                     str(addr[1]) + " Clients name is already in use. "
                 conn.send(send_data.encode(FORMAT))
-                #clients.remove(data[1])
+                # clients.remove(data[1])
                 break
 
-        elif cmd == "DE-REGISTER": #If client wants to deregister
-            clientCount -= 1 #Decrement the client count
-            if name in clients: #If the name exists in clients list
-                clients.remove(name) #Remove the name from the list
+        elif cmd == "DE-REGISTER":  # If client wants to deregister
+            clientCount -= 1  # Decrement the client count
+            if name in clients:  # If the name exists in clients list
+                clients.remove(name)  # Remove the name from the list
                 send_data = "OK@"
-                send_data += "De-Registered successfully" #What to send back to the user
+                send_data += "De-Registered successfully"  # What to send back to the user
                 name = data[1]
                 print("[DE-REGISTERED] ", str(addr), " ", str(data[1]))
                 conn.send(send_data.encode(FORMAT))
                 break
-            else: #if the client that wants to deregister enters the wrong name
+            else:  # if the client that wants to deregister enters the wrong name
                 print("That client is not connected. Please try again !")
                 send_data = "NO@"
                 conn.send(send_data.encode(FORMAT))
@@ -141,17 +159,20 @@ def handle_client(conn, addr): #Handles client request and response
 
 
 def main():
-    print('\033[1m', Fore.CYAN + " \n \n - 💻 - WELCOME SERVER  - 💻 - \n \n") #Welcome message for the server
+    # Welcome message for the server
+    print('\033[1m', Fore.CYAN + " \n \n - 💻 - WELCOME SERVER  - 💻 - \n \n")
     print("[STARTING] Server is starting. ")
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Connect the server to a socket
-    server.bind(ADDR) #Bind the Ip and the Port
-    server.listen() #Listen for a client
+    # Connect the server to a socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)  # Bind the Ip and the Port
+    server.listen()  # Listen for a client
     print("[Listening] Server is listening. \n \n")
 
     while 1:
-        conn, addr = server.accept() #Accepting a client
-        thread = threading.Thread(target=handle_client, args=(conn, addr)) #Making a client a thread
-        thread.start() #Starting the thread and connecting the client
+        conn, addr = server.accept()  # Accepting a client
+        thread = threading.Thread(target=handle_client, args=(
+            conn, addr))  # Making a client a thread
+        thread.start()  # Starting the thread and connecting the client
 
 
 if __name__ == "__main__":
